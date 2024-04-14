97822d05a858be05e3abd287241bb17715732d6b
Michael Wood <michaelwood33311@icloud.com>
2024-04-14 09:31:09 -0400

    Styled package selection and added top label

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import { infoSetTables } from './store/sliceInfo';
 
 function App() {
   const dispatch = useDispatch();
@@ -12,6 +13,8 @@ function App() {
   const getTableInfo = tableName => {
     console.log(tableName);
   }
+
+  let accuracy = 0;
 
   useEffect(() => {
    axios.get("https://acurai.ai:5100/tables")
@@ -25,9 +28,9 @@ function App() {
   }, [])
   return (
     <>
-      <h1 className="text-blue-600">Hello</h1>
+      <h1 className="text-blue-600 mb-4">Accuracy {accuracy}%</h1>
       <div className="selectContainer">
-        <select name="tables" id="tables" onChange={e => getTableInfo(e.target.value)}>
+        <select name="tables" id="tables" onChange={e => getTableInfo(e.target.value)} className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           {info.tables.map((t, i) => {
             return <option key={t} value={t}>{t}</option>
           })}
