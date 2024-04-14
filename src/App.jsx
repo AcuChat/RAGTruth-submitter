1b14341e525f68bbc0313ef3704b9ae95d30ac36
Michael Wood <michaelwood33311@icloud.com>
2024-04-14 09:43:14 -0400

    added getting new data upon table selection

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     <>
       <h1 className="text-blue-600 mb-4">Accuracy {accuracy}%</h1>
       <div className="selectContainer">
-        <select name="tables" id="tables" onChange={e => getTableInfo(e.target.value)} className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select name="tables" id="tables" onChange={e => getData(e.target.value)} className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           {info.tables.map((t, i) => {
             return <option key={t} value={t}>{t}</option>
           })}
